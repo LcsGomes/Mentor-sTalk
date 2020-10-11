148eee4dbda29760b3cf0059731c5e11d3264f8d
Lucas Gomes <Djlucas10@hotmail.com>
2020-10-11 15:25:04 -0300

    V1 da News

--- MentorsTalk/src/app/home/news/news.page.ts
+++ MentorsTalk/src/app/home/news/news.page.ts
@@ -8,6 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsPage implements OnInit {
 
+  public noticias = [
+    {
+      Titulo:'Update',
+      Sub:'Version 1.0',
+      Descricao:'Atualização 1.0, Inclusão das paginas News, Agenda, Catalago, Detail, App e melhoria da perfomance',
+      Att:'Att,',
+      Ass: 'Grupo10'
+    },
+    {
+      Titulo:'Novidades',
+      Sub:'Em breve',
+      Descricao:'Inclusão da possibilidade de efetuar loggin e agendar Mentores',
+      Att:'Att,',
+      Ass: 'Grupo10'
+    }
+  ]
   public Mentoresnovos = [
     {
       id:1,
