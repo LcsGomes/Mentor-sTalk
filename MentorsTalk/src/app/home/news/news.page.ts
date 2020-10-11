7ee1937e5627f999da2b14c8554578a2a1479de5
Lucas Gomes <Djlucas10@hotmail.com>
2020-10-11 14:49:02 -0300

    Inclusão do Like e Favoritos

--- MentorsTalk/src/app/home/news/news.page.ts
+++ MentorsTalk/src/app/home/news/news.page.ts
@@ -1,3 +1,4 @@
+import { identifierModuleUrl } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -11,30 +12,59 @@ export class NewsPage implements OnInit {
     {
       id:1,
       Nome:'Ana Maria',
-      Cargo:'Cientista de Dados'
+      Cargo:'Cientista de Dados',
+      liked:false
     },
     {
       id:2,
       Nome:'Adriana Gomes',
-      Cargo:'Desenvolvedor Full Stack'
+      Cargo:'Full Stack',
+      liked:false
     },
     {
       id:3,
       Nome:'Luisa Lopes',
-      Cargo:'Programador C#'
+      Cargo:'Programador C#',
+      liked:false
     },
     {
       id:4,
       Nome:'Tayna Oliveira',
-      Cargo:'Scrum Master'
+      Cargo:'Scrum Master',
+      liked:false
     },
     {
       id:5,
       Nome:'Tamires Santos',
-      Cargo:'Desenvolvedora de Apps'
+      Cargo:'Desenvolvedora de Apps',
+      liked:false
     }
   ]
 
+
+  public favoritos = [ 
+  ] 
+
+
+  public addFavoritos(id: number){
+    const index = this.Mentoresnovos.findIndex(function (t){
+      return t.id == id;
+    });  
+    
+    if(this.Mentoresnovos[index].liked == false)  
+    {
+      this.Mentoresnovos[index].liked = true;
+      this.favoritos.push(this.Mentoresnovos[index])
+    }
+    else
+    {
+      this.Mentoresnovos[index].liked = false;  
+      const index2  = this.favoritos.findIndex(function (t){
+        return t.id == id;
+      });  
+      this.favoritos.splice(index2,1)
+    }
+  }
   constructor() { }
 
   ngOnInit() {
