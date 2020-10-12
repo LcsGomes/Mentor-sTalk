ebb3c0e7eaafbfb6c8e00a76089cc4a5eeb5e5ab
Lucas Gomes <Djlucas10@hotmail.com>
2020-10-12 20:38:57 -0300

    desenvolvimento da Pag quemsomos V1

--- MentorsTalk/src/app/home/quemsomos/quemsomos.page.ts
+++ MentorsTalk/src/app/home/quemsomos/quemsomos.page.ts
@@ -7,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuemsomosPage implements OnInit {
 
+  public equipe = [
+  {
+    Nome:'Lucas Gomes',
+    RA:'21290877'
+  },
+  {
+    Nome:'Andrey Henrique',
+    RA:'21163712'
+  },
+  {
+    Nome:'Paulo Ricardo',
+    RA:'21081948'
+  },
+  {
+    Nome:'Gilmar Victor',
+    RA:'21174800'
+  },
+  {
+    Nome:'Vitor Martins',
+    RA:'21112568'
+  }
+  ]
   constructor() { }
 
   ngOnInit() {
